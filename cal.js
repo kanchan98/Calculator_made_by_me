3e09efe07b3ba248bbaa6303aaf2d703b77ab5e0
Your Name <kanchannanawaredac98@gmail.com>
2019-01-13 11:05:32 +0530

    first commit

---
+++ cal.js
@@ -0,0 +1,22 @@
+var a;
+var b;
+function aDd(a,b)
+{
+    return (a + b);
+}
+
+function sUb(a,b) 
+{
+    return (a - b);
+}
+
+function mUl(a,b)
+{
+    return (a * b);
+}
+
+function dIv(a,b)
+ {
+    return (a / b);
+}
+
